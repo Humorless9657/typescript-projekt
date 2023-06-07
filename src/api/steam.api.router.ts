caef9389669780f517813866e01994ea77c2a80d
Puc3k <kacperplocienniczak20@gmail.com>
2023-06-07 20:41:16 +0200

    feat: add api steam apps list endpoint

--- src/api/steam.api.router.ts
+++ src/api/steam.api.router.ts
@@ -4,8 +4,21 @@ import {SteamApiService} from './steam.api.service';
 
 const apiService = new SteamApiService('https://store.steampowered.com/api/appdetails?appids=');
 const steamApiRouter = express.Router();
+// GET: A list of steam apps
+steamApiRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const appList = await apiService.getAppList();
+        if (appList) {
+            return res.status(200).json(appList);
+        } else {
+            return res.status(404).json(`Can't find list of apps from Steam API.`);
+        }
+    } catch {
+        return res.status(500);
+    }
+})
 
-// GET: A single Game from Steam by Id
+// GET: A single Game from Steam by ID
 steamApiRouter.get('/:gameId', async (req: Request, res: Response) => {
     const gameId: number = parseInt(req.params.gameId, 10);
 
--- src/api/steam.api.service.ts
+++ src/api/steam.api.service.ts
@@ -1,10 +1,25 @@
 import axios, {AxiosResponse} from 'axios';
 
 export class SteamApiService {
-    private apiUrl: string;
+    private readonly apiUrl: string;
 
     constructor(apiUrl: string) {
         this.apiUrl = apiUrl;
+    }
+
+    public async getAppList(): Promise<any> {
+        const appListUrl = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
+
+        try {
+            const response: AxiosResponse = await axios.get(appListUrl);
+            if (!response.data) {
+                return false;
+            }
+            return response.data;
+        } catch (error) {
+            console.error('Błąd podczas pobierania danych:', error);
+            throw error;
+        }
     }
 
     public async getGameById(gameId: number): Promise<any> {
@@ -16,12 +31,15 @@ export class SteamApiService {
                 return false;
             }
             const responseData = response.data[gameId]['data'];
-            const selectedData = {
+            return {
                 name: responseData.name,
                 pc_requirements: responseData.pc_requirements,
-                languages: responseData.supported_languages
+                languages: responseData.supported_languages,
+                developers: responseData.developers,
+                price_overview: responseData.price_overview,
+                categories: responseData.categories,
+                release_date: responseData.release_date,
             };
-            return selectedData;
         } catch (error) {
             console.error('Błąd podczas pobierania danych:', error);
             throw error;
